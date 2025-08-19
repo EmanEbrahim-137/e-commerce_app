6b25e76790c85e0c7cace8d0f20de44b8d5ae39d
Eman <eman@gmail.com>
2025-08-19 17:28:09 +0200

    new branch

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -117,3 +117,21 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// class ProductDetailScreen extends StatefulWidget {
+//   const ProductDetailScreen({super.key});
+
+//   @override
+//   State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+// }
+
+// class _ProductDetailScreenState extends State<ProductDetailScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: animated,
+//             );
+//           }
+//         }
